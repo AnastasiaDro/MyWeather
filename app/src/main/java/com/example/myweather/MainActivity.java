478d19ab7e28321b1bbd7de9c3181d12d8e6ad5a
AnastasiaDro <adrogunova@gmail.com>
2019-08-16 18:54:31 +0300

    сделан второй экран сделан java-класс для него и активность прописана в манифесте *чётко* на главной активности добавлна кнопка перехода на второй экран

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //обработчик нажатия на кнопку change - поменять город
+    //переходим на второй экран с activity_city
+    public void onChangeClick(View view) {
+        Intent intent = new Intent(MainActivity.this, CityActivity.class);
+        startActivity(intent);
     }
 }
