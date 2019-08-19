be0985a55c8dc50b6c7b871cd1619fe7d7631e57
AnastasiaDro <adrogunova@gmail.com>
2019-08-19 22:58:27 +0300

    -Добавлен переход на второй экран -улучшены расположения элементов на втором экране -настроена передача данных между экранами
    ДОРАБОТАТЬ:
    -ориентация landscape для активностей, т.к. на телефоне утекают кнопки
    -добавить сохранение данных при повороте экрана

--- app/src/main/java/com/example/myweather/CityActivity.java
+++ app/src/main/java/com/example/myweather/CityActivity.java
@@ -1,13 +1,53 @@
 package com.example.myweather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class CityActivity extends Activity {
+
+
+    public final static String CITY = "ru.anastasiadrogunova.myweather.CITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_city);
+
+
+        //возвращаем информацию о городе в MainActivity
+        final Intent answerIntent = new Intent();
+        final EditText editText =  findViewById(R.id.editText);
+        editText.setOnKeyListener(new View.OnKeyListener()
+                                  {
+                                      public boolean onKey(View v, int keyCode, KeyEvent event)
+                                      {
+                                          if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                                                  (keyCode == KeyEvent.KEYCODE_ENTER))
+                                          {
+                                              //сохраняем текст, введенный до нажатия Enter в переменную
+                                              String myCityStr = editText.getText().toString();
+                                              answerIntent.putExtra(CITY, myCityStr);
+                                              setResult(RESULT_OK, answerIntent);
+                                              finish();
+                                              return true;
+                                          }
+                                          return false;
+                                      }
+                                  }
+        );
+
+
+
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,22 +1,108 @@
 package com.example.myweather;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    static final private int CHOOSE_CITY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //тренируемся в воспроизведении жизенных циклов активити
+        String instanceState;
+        if (savedInstanceState == null){
+            instanceState = getString(R.string.first_start);
+        }
+        else {
+            instanceState = "Повторый запуск";
+        }
+        Toast.makeText(getApplicationContext(), instanceState, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+
     }
 
     //обработчик нажатия на кнопку change - поменять город
-    //переходим на второй экран с activity_city
+    //переходим на второй экран с activity_city и ожидаем ввода города
     public void onChangeClick(View view) {
         Intent intent = new Intent(MainActivity.this, CityActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, CHOOSE_CITY);
     }
+
+    //Ждём название города из CityActivity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        TextView textViewCity = findViewById(R.id.textViewCity);
+
+        if (requestCode == CHOOSE_CITY) {
+            if (resultCode == RESULT_OK) {
+                String cityName = data.getStringExtra(CityActivity.CITY);
+                textViewCity.setText(cityName);
+            } else {
+                textViewCity.setText(R.string.defaultCity);//ставим значение по умолчанию
+            }
+        }
+
+    }
+
+
 }
